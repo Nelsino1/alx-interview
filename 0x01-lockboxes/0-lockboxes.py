087da8ee5703ecd712c8889e8171375a0d555171
Nelsino1 <samuelnelsino@gmail.com>
2023-07-04 07:42:54 -0700

    add

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-def canUnlockAll(boxes):
+def can_unlock_all(boxes):
     if len(boxes) == 0:
         return False
 
@@ -20,10 +20,11 @@ def canUnlockAll(boxes):
 
 # Test cases
 boxes = [[1], [2], [3], [4], []]
-print(canUnlockAll(boxes))  # True
+print(can_unlock_all(boxes))  # True
 
 boxes = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
-print(canUnlockAll(boxes))  # True
+print(can_unlock_all(boxes))  # True
 
 boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
-print(canUnlockAll(boxes))  # False
+print(can_unlock_all(boxes))  # False
+
